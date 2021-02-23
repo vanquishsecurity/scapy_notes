053e6e24d261ccd220121e8928f812d533409644
Vanquish Security <vanquish.security@gmail.com>
2021-02-23 03:59:46 -0500

    Update ipv4_three_way_handshake.py

--- ipv4_three_way_handshake.py
+++ ipv4_three_way_handshake.py
@@ -1,21 +1,14 @@
 from scapy.all import *
 
 sport = random.randint(1024,65535)
-dport = 80
+dport = XXXXX
 
 # SYN
-ip=IP(src="192.168.1.123", dst="10.10.0.123")
-TCP_SYN=TCP(sport=sport, dport=dport, flags="S", seq=1000)
+ip=IP(src='X.X.X.X',dst='X.X.X.X')
+SYN=TCP(sport=sport,dport=XXXXX,flags='S',seq=1000)
+SYNACK=sr1(ip/SYN)
 
 # SYN-ACK
-TCP_SYNACK=sr1(ip/TCP_SYN)
+ACK=TCP(sport=sport, dport=XXXXX, flags='A', seq=SYNACK.ack + 1, ack=SYNACK.seq + 1)
 
-# ACK
-my_ack = TCP_SYNACK.seq + 1
-TCP_ACK=TCP(sport=sport, dport=dport, flags="A", seq=TCP_SYNACK.ack, ack=my_ack)
-send(ip/TCP_ACK)
-
-# PUSH
-payload = "test"
-TCP_PUSH=TCP(sport=sport, dport=dport, flags="PA", seq=TCP_SYNACK.ack, ack=my_ack)
-send(ip/TCP_PUSH/payload)
+send(ip/ACK, count=5)
